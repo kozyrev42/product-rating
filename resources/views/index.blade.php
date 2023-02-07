d54e0a38519ba9c27bb2d927cf568ceeacb81f85
kozyrev42 <kozyrev054@gmail.com>
2023-02-08 00:01:46 +0700

    сделал UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,68 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		<title>Рейтинг товаров</title>
-	</head>
-	<body>
-		<h5>Главная страница</h5>
-	</body>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Рейтинг товаров</title>
+	<link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
+	<h1>Рейтинг товаров</h1>
+	<div>
+		<a href="/registration">Регистрация</a>
+	</div>
+	<div>
+		<a href="/login">Вход</a>
+	</div>
+	<div>
+		<a href="">Выйти</a>
+	</div>
+	<br>
+	<div>
+		<a href="/createfakes">Создать фейк-товары</a>
+	</div>
+
+	<br><hr><br>
+	
+
+	<div>
+		<a href="">Рейтинг товаров по возрастанию</a>
+	</div>
+	<div>
+		<a href="">Рейтинг товаров по убыванию</a>
+	</div>
+	<div>
+		<a href="">Товары с моими отзывами</a>
+	</div>
+
+	<br><br>
+
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Наименование</th>
+				<th>Рейтинг</th>
+				<th class="review">Отзывы</th>
+				<th>Оставить отзыв</th>
+				<th>Редактировать отзыв</th>
+				<th>Удалить отзыв</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>круг</td>
+				<td>3</td>
+				<td class="review">текст отзыва мммммммммммммм  мммммммммммм</td>
+				<td>оставить отзыв</td>
+				<td>редактировать отзыв</td>
+				<td>удалить отзыв</td>
+			</tr>
+		</tbody>
+	</table>
+
+
+
+
+</body>
 </html>
